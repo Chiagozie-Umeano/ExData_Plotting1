a643a8244b682b305f3b5c0cd63fce92326575c1
Chiagozie Umeano <67871741+Chiagozie-Umeano@users.noreply.github.com>
2020-09-25 11:40:31 +0100

    PlotR1

--- Plot R 1.R
+++ Plot R 1.R
@@ -5,7 +5,7 @@
   data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
   hist(data1$Global_active_power, main="Global Active Power", 
        +      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
-  png("plot1.png", width=480, height=480)
+  dev.copy(png,file="plot1.png", width=480, height=480)
     dev.off() 
 }
       
